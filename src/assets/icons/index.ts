5524cde4277eceef3b020f7a656abe0a6460ac81
Nitesh <imnksid@gmail.com>
2026-03-29 18:39:47 +0530

    Added SVG icons & it's index export file

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,27 @@
+import bulb_icon from "./bulb_icon.svg";
+import code_icon from "./code_icon.svg";
+import compass_icon from "./compass_icon.svg";
+import gallery_icon from "./gallery_icon.svg";
+import history_icon from "./history_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import message_icon from "./message_icon.svg";
+import mic_icon from "./mic_icon.svg";
+import plus_icon from "./plus_icon.svg";
+import question_icon from "./question_icon.svg";
+import send_icon from "./send_icon.svg";
+import setting_icon from "./setting_icon.svg";
+
+export const ICONS = {
+  bulb_icon,
+  code_icon,
+  compass_icon,
+  gallery_icon,
+  history_icon,
+  menu_icon,
+  message_icon,
+  mic_icon,
+  plus_icon,
+  question_icon,
+  send_icon,
+  setting_icon,
+};
