5c67f3c706b222908b8b8834d581a45c2b234145
Nitesh <imnksid@gmail.com>
2026-03-29 18:40:23 +0530

    Added PNG images & an index file to export

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import gemini_image from "./gemini_image.png";
+import user_image from "./user_image.png";
+import youtube_image from "./youtube_image.png";
+
+export const IMAGES = {
+  gemini_image,
+  user_image,
+  youtube_image,
+};
